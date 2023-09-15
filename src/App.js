f41263ba6a997713ffdd3ad0b5694f94d3b31195
783R <144540374+783R@users.noreply.github.com>
2023-09-15 16:49:46 +0530

    Modified expenses

--- src/App.js
+++ src/App.js
@@ -4,22 +4,27 @@ function App() {
   const expenses = [
     {
       id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
+      title: "Groceries",
+      amount: 75.36,
+      date: new Date(2023, 8, 5),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e2",
+      title: "Internet Bill",
+      amount: 89.99,
+      date: new Date(2023, 8, 10),
+    },
     {
       id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
+      title: "Dinner Out",
+      amount: 45.75,
+      date: new Date(2023, 8, 18),
     },
     {
       id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
+      title: "Gasoline",
+      amount: 40.2,
+      date: new Date(2023, 8, 22),
     },
   ];
 
